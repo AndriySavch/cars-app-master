107a73c69b93a80a8ba95f77705e042461adfc4f
Andriy <andry10042000@gmail.com>
2018-08-25 12:13:27 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+
+import { ROUTES } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { CarCardComponent } from './components/car-card/car-card.component';
+import { PricePipe } from './pipes/price.pipe';
+import { ShortenPipe } from './pipes/shorten.pipe';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CarListComponent } from './pages/car-list/car-list.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CreateCarComponent } from './pages/create-car/create-car.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarCardComponent,
+    PricePipe,
+    ShortenPipe,
+    MenuComponent,
+    LoginComponent,
+    CarListComponent,
+    CreateUserComponent,
+    FavoritesComponent,
+    HomeComponent,
+    SideBarComponent,
+    LoaderComponent,
+    NotFoundComponent,
+    CreateCarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {
+      useHash: Boolean(history.pushState) === false,
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CarListComponent } from './pages/car-list/car-list.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { CreateCarComponent } from './pages/create-car/create-car.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateUserComponent },
+  { path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: '/car-list',  pathMatch: 'full'},
+      { path: 'car-list', component: CarListComponent },
+      { path: 'create-car', component: CreateCarComponent},
+      { path: 'favorites', component: FavoritesComponent }
+    ]
+  },
+  {path: '**', component: NotFoundComponent }
+];
